592c869f01de98a48e74d9e3f96568398f6c0a6a
ivkhl <khlaponin.dp@gmail.com>
2021-02-15 19:38:24 +0100

    Площадь треугольника. [#459071]

--- src/ru/job4j/condition/TrgArea.java
+++ src/ru/job4j/condition/TrgArea.java
@@ -4,9 +4,8 @@ public class TrgArea {
     public static double area(double a, double b, double c) {
         double hprm = (a + b + c) /2;
         return Math.sqrt(hprm * (hprm - a) * (hprm - b) * (hprm - c));
-        // hprm means halfperimeter
+        //  hprm means halfperimeter
     }
-
     public static void main(String[] args) {
         double rsl = TrgArea.area(2, 2, 2);
         System.out.println("area (2, 2, 2) = " + rsl);
